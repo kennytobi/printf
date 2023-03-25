33b8173348684ccfadf85367e6516cacd2d02232
kennytobi <kennytobi@github.com>
2023-03-25 04:27:35 -0700

    group

--- _printf.c
+++ _printf.c
@@ -13,7 +13,7 @@ int _printf(const char *format, ...)
 	if (format == NULL)
 		return (-1);
 	va_start(args, format);
-	while (format && format[i] != '\0')
+	while (format && format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
 		{
